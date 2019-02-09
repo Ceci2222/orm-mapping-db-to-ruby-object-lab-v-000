d67512a72af6b1f2418c7704c1e2b98bd841bf2b
Marie Burns <mcburns2222@gmail.com>
2019-02-09 01:33:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,9 +22,7 @@ class Student
   
   
     
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
-  end
+
 
   def self.find_by_name(name)
     # find the student in the database given a name
